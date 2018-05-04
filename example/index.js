61d628b644a419797449911bbd47dc40dd25f519
Piotr Krawiec <piotr@krawiec.me>
2018-05-04 13:50:29 +0200

    Update the example with the new, scoped package

--- example/index.js
+++ example/index.js
@@ -4,7 +4,7 @@
 import { AppRegistry } from 'react-native'
 import App from './App'
 
-import * as RNEP from 'react-native-estimote-proximity'
+import * as RNEP from '@estimote/react-native-proximity'
 
 AppRegistry.registerComponent('example', () => App)
 
